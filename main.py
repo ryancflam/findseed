f4de7d676fecf9a776bbfe1da0e736588261db18
Ryan <ryancflam@hotmail.com>
2020-12-23 21:12:10 +0800

    autogenerates blacklist.json and data.json also moved bot.py to main directory

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 #!/usr/bin/python3
 
+from os import path
+from json import dump
+from time import time
 from asyncio import get_event_loop as loop
 
 import info
-from other_utils.bot import Bot
+from bot import Bot
 from other_utils.funcs import getPath
+
+if not path.exists(f"{getPath()}/blacklist.json"):
+    f = open(f"{getPath()}/blacklist.json", "w")
+    dump({"servers": [], "users": []}, f, sort_keys=True, indent=4)
+    f.close()
+
+if not path.exists(f"{getPath()}/data.json"):
+    f = open(f"{getPath()}/data.json", "w")
+    dump({
+        "calls": 0,
+        "highest": {
+            "found": 0,
+            "number": 0,
+            "time": int(time())
+        }
+    }, f, sort_keys=True, indent=4)
+    f.close()
 
 client = Bot(
     loop=loop(),
