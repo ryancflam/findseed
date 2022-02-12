67a328e8ba1eb5dde98f4d61f433946b20f56613
Ryan <ryancflam@hotmail.com>
2022-02-13 00:39:54 +0800

    now 4am.mp3 plays in the background

--- resources/web_server/static/script.js
+++ resources/web_server/static/script.js
@@ -1,22 +1,17 @@
 const t = new TimelineMax();
-t.fromTo(document.querySelector(".banner-img"), 2, {height: "0%"}, {height: "100%", ease: Power2.easeInOut});
+const img = document.querySelector(".banner-img");
+t.fromTo(img, 2, {height: "0%"}, {height: "100%", ease: Power2.easeInOut});
 document.addEventListener("contextmenu", event => event.preventDefault());
 
-function playWhenReady() {
-    var d = new Date();
-    var h = d.getHours();
-    var m = d.getMinutes();
-    var s = d.getSeconds();
+function play4AM() {
+    var date = new Date();
+    var h = date.getHours();
+    var m = date.getMinutes();
+    var s = date.getSeconds();
     if (h === 4 && m === 0 && s === 0) {
-        playSound("static/assets/4am.mp3");
-        clearInterval(interval);
+        document.getElementById("audio").play();
     }
 }
 
-function playSound(soundFile) {
-    var audioElement = document.createElement("audio");
-    audioElement.setAttribute("src", soundFile);
-    audioElement.play();
-}
-
-var interval = setInterval("playWhenReady()", 1000);
+const interval = 1000;
+setInterval("play4AM()", interval);
