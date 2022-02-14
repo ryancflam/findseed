841139696a97282f7c4a397a18a31c08a0411f50
Ryan <ryancflam@hotmail.com>
2022-02-15 01:50:49 +0800

    script.js - formatTime() and logTime()

--- resources/web_server/static/script.js
+++ resources/web_server/static/script.js
@@ -3,11 +3,20 @@ const img = document.querySelector(".banner-img");
 t.fromTo(img, 2, {height: "0%"}, {height: "100%", ease: Power2.easeInOut});
 document.addEventListener("contextmenu", event => event.preventDefault());
 
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+}
+
+function logTime(h, m, s) {
+    console.log("Current time - " + formatTime(h) + ":" + formatTime(m) + ":" + formatTime(s));
+}
+
 function main() {
     var date = new Date();
     var h = date.getHours();
     var m = date.getMinutes();
     var s = date.getSeconds();
+    logTime(h, m, s);
     if (h === 4 && m === 0 && s === 0) {
         document.getElementById("audio").play();
     }
