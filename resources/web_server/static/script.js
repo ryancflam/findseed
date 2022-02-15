f2addc791ffba171d1c7a5ef4ac7a45a122b8b56
Ryan <ryancflam@hotmail.com>
2022-02-15 18:16:46 +0800

    cross browser audio playing

--- resources/web_server/static/script.js
+++ resources/web_server/static/script.js
@@ -3,13 +3,36 @@ const img = document.querySelector(".banner-img");
 t.fromTo(img, 2, {height: "0%"}, {height: "100%", ease: Power2.easeInOut});
 document.addEventListener("contextmenu", event => event.preventDefault());
 
+const audioURL = "static/assets/4am.mp3";
+var AudioContext = window.AudioContext || window.webkitAudioContext;
+const ctx = new AudioContext();
+let audio;
+
+fetch(audioURL)
+    .then(data => data.arrayBuffer())
+    .then(arrayBuffer => ctx.decodeAudioData(arrayBuffer))
+    .then(decodedAudio => {
+        audio = decodedAudio;
+    });
+
+
 function formatTime(time) {
     return time < 10 ? `0${time}` : time;
 }
 
+
 function logTime(h, m, s) {
     console.log("Current time - " + formatTime(h) + ":" + formatTime(m) + ":" + formatTime(s));
 }
+
+
+function playSound() {
+    var sound = ctx.createBufferSource();
+    sound.buffer = audio;
+    sound.connect(ctx.destination);
+    sound.start();
+}
+
 
 function main() {
     var date = new Date();
@@ -18,9 +41,10 @@ function main() {
     var s = date.getSeconds();
     logTime(h, m, s);
     if (h === 4 && m === 0 && s === 0) {
-        document.getElementById("audio").play();
+        playSound();
     }
 }
+
 
 const interval = 1000;
 setInterval(main, interval);
