3a489060fec7fc4c87723d60e460e5310575e865
Ryan <ryancflam@hotmail.com>
2022-02-15 22:04:39 +0800

    ok then

--- resources/web_server/static/script.js
+++ resources/web_server/static/script.js
@@ -3,6 +3,17 @@ const img = document.querySelector(".banner-img");
 t.fromTo(img, 2, {height: "0%"}, {height: "100%", ease: Power2.easeInOut});
 document.addEventListener("contextmenu", event => event.preventDefault());
 
+
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+}
+
+
+function logTime(h, m, s) {
+    console.log("Current time - " + formatTime(h) + ":" + formatTime(m) + ":" + formatTime(s));
+}
+
+
 const audioURL = "static/assets/4am.mp3";
 var AudioContext = window.AudioContext || window.webkitAudioContext;
 const ctx = new AudioContext();
@@ -23,16 +34,6 @@ function unlockAudioContext() {
     function clean() {
         events.forEach(e => b.removeEventListener(e, unlock));
     }
-}
-
-
-function formatTime(time) {
-    return time < 10 ? `0${time}` : time;
-}
-
-
-function logTime(h, m, s) {
-    console.log("Current time - " + formatTime(h) + ":" + formatTime(m) + ":" + formatTime(s));
 }
 
 
